72bf1857b002a098abef7d8801f313a0f1de2528
Ivan Mazuryk <ivan.mazk@gmail.com>
2018-10-17 21:16:33 +0200

    Add project files.

---
+++ WebApplication2/Models/Photo.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+using WebApplication2.Models.School;
+
+namespace WebApplication2.Models
+{
+    public class Photo
+    {
+        [Key]
+        public int PhotoId { get; set; }
+
+        public string FilePath { get; set; }
+
+        public byte[] Image { get; set; }
+
+        public int StudentId { get; set; }
+
+        public Student Student { get; set; }
+
+    }
+}
